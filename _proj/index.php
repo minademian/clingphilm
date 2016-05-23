1105473f11d2b3ae9b497d5512275b6f9f6ac3a8
Mina Demian <work_t@midiane.com>
2016-05-23 03:29:02 +0200

    oops. uncomment stylesheet link

--- _proj/index.php
+++ _proj/index.php
@@ -3,7 +3,7 @@
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
   <title>ClingFilm - a tool to protect your photos</title>
-<!--   <link rel="stylesheet" href="clingfilm.css" media="screen" type="text/css" /> -->
+  <link rel="stylesheet" href="clingfilm.css" media="screen" type="text/css" />
 </head>
 <body>
   <div id="clingfilm">
