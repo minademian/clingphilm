2bceeadd10687f44d8bb34e7b3640fb9cabb7311
Mina Demian <work_t@midiane.com>
2016-05-23 03:14:41 +0200

    clean up. dead end for now because can't get around disabling CSS. whole solution is in the CSS. without it the browser renders elements sequentially.

--- _proj/index.php
+++ _proj/index.php
@@ -3,30 +3,21 @@
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
   <title>ClingFilm - a tool to protect your photos</title>
-  <link rel="stylesheet" href="clingfilm.css" media="screen" type="text/css" />
+<!--   <link rel="stylesheet" href="clingfilm.css" media="screen" type="text/css" /> -->
 </head>
 <body>
   <div id="clingfilm">
-    <form method="post" action="index.php" onsubmit="Warn();">
     <?php
     for($i=0;$i<5000;$i++)
     {
     ?>
-      <input type="submit" value="" name="ass" />
+      <input type="button" value="" name="ass" />
     <?php
     }
     ?>
-    </form>
   </div>
   <div id="content">
-    <?php
-    for($i=0;$i<100;$i++)
-    {
-    ?>
-      <img src="./artwork/heartfelt-sadness.jpg" alt="heartfelt sadness" />
-    <?php
-    }
-  ?>
+    <img src="./artwork/heartfelt-sadness.jpg" alt="heartfelt sadness" />
   </div>
 </body>
 </html>
