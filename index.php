2f55b069f76f3085f3db017ed4e4011fdb5a4305
Mina Demian <work_t@midiane.com>
2016-06-08 20:56:37 +0200

    cleanup

--- index.php
+++ index.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta charset="UTF-8">
   <title>ClingFilm - a tool to protect your photos</title>
   <link rel="stylesheet" href="clingfilm.css" media="screen" type="text/css" />
 </head>
 <body>
   <div id="clingfilm">
     <?php
-    for($i=0;$i<5000;$i++)
+    for($i=0;$i<500;$i++)
     {
     ?>
-      <input type="button" value="" name="ass" />
+      <button type="button" value="" name="protection" />
     <?php
     }
     ?>
   </div>
   <div id="content">
-    <img src="./artwork/heartfelt-sadness.jpg" alt="heartfelt sadness" />
+    <img src="./_assets/heartfelt-sadness.jpg" alt="heartfelt sadness" />
   </div>
 </body>
 </html>
